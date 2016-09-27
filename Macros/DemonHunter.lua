da66e8897005d1d39dba0ee6c870d451ce1ddb2f
Killatera <r9516865190@gmail.com>
2016-09-27 17:17:12 +0300

    Update DemonHunter.lua

--- Macros/DemonHunter.lua
+++ Macros/DemonHunter.lua
@@ -3,3 +3,30 @@ local Sequences = GSMasterSequences
 ------------------
 ----- Demon Hunter
 ------------------
+Sequences["DH"] = {
+StepFunction = [[
+	limit = limit or 1
+	if step == limit then
+		limit = limit % #macros + 1
+		step = 1
+	else
+		step = step % #macros + 1
+	end
+]],
+PreMacro = [[
+/cast [combat] Demon Spikes
+/targetenemy [noharm][dead]
+]],
+"/cast !Shear",
+"/cast !Soul Carver",
+"/cast !",
+"/cast !Fel Eruption",
+"/cast !",
+PostMacro = [[
+/cast [Combat]Immolation Aura
+/cast [Combat]Empower Wards
+/cast Fiery Brand
+/startattack
+/script UIErrorsFrame:Hide();
+]],
+}
